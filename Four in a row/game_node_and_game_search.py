402b641e50f0b8a69b1bfe906018589873e95d70
iamshirdi <iamshirdi@gmail.com>
2021-09-27 12:22:56 +0200

    Add Monte- Carlo Skelton Code

--- Four in a row/game_node_and_game_search.py
+++ Four in a row/game_node_and_game_search.py
@@ -13,9 +13,15 @@ class GameNode:
     This class defines game nodes in game search trees. It keep track of: 
     state
     '''
-    def __init__(self, state, node=None):
+    def __init__(self, state, node=None, parent=None):
         self.state = state   
-           
+        self.actions_left = []
+        self.visits = 0
+        self.wins = 0
+        self.parent = parent
+        self.succesors=[]
+
+
 class GameSearch:
     '''
     Class containing different game search algorithms, call it with a defined game/node
@@ -24,6 +30,7 @@ class GameSearch:
         self.state = game       
         self.depth = depth
         self.time = time
+
     def mcts(self):                     
         start_time = process_time() 
         tree = GameNode(self.state)
@@ -38,6 +45,68 @@ class GameSearch:
             elapsed_time = stop_time - start_time
         move = self.actions(tree)
         return move
+    
+    def select(self, tree):
+        # if any actions left/unexplored its ucb is infinite we choose it
+        if len(tree.actions_left) > 0:
+            a = random.choice(tree.actions_left)
+            tree.actions_left.pop(a)
+            s_node = GameNode(tree.state.result(a), parent = tree)
+            tree.succesors.append(s_node)
+            return s_node
+        
+        #should not be terminal node return
+        node = None
+        ucb = float('-inf')
+        for child_gn in tree.successors:
+            ucb2 = child_gn.wins/child_gn.visits + 1.4 * ((math.log(tree.wins)/child_gn.visits)**0.5)
+            if ucb2 > ucb :
+               node = child_gn
+        return node # returns highest ucb node if not visited/explored as selected child node
+
+    def expand(self, leaf):
+        if leaf.state.is_terminal:
+            return leaf
+        if len(leaf.actions_left) < 1:
+            leaf.actions_left = leaf.state.actions()   
+
+        s_node =self.select(leaf)
+        return s_node
+
+    def simulate(self, child):
+        while True:
+            if child.state.is_terminal:
+                return child
+            if len(child.actions_left) < 1:
+                child.actions_left = child.state.actions()
+
+            child = self.select(child)
+
+    def back_propagate(self, result, child):
+        if result.state.curr_move == result.state.ai_player:
+            win = True
+        else:
+            win = False
+        while child.parent != None: #even middle state tree parent is None when initalized
+            child.visits +=1
+            if win:
+                child.wins +=1
+            win = not win
+            child = child.parent
+
+    def actions(self, tree):
+        node = None # terminal node must not be
+        ucb = float('-inf')
+        for child_gn in tree.successors:
+            ucb2 = child_gn.wins/child_gn.visits + 1.4 * ((math.log(tree.wins)/child_gn.visits)**0.5)
+           #incase initial selection is not done check if no wins
+            if (child_gn.wins or tree.wins ==0) and child_gn.visits==0:
+                if ucb2 > ucb :
+                    node = child_gn
+        #since we did not keep track of move for a action in node
+        for a in tree.state.actions():
+            if node.state == tree.state.result(a):
+                return a
     
     def minimax_search(self): 
         start_time = process_time()   
